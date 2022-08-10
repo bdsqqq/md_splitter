ae694303840c9b0b079ddf5c6906891873a68f8a
Igor Bedesqui <igorbedesqui@gmail.com>
2022-08-10 13:01:34 -0300

    feat: create .md in /out dir

--- mdSplitter.go
+++ mdSplitter.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 )
 
+// Thanks https://stackoverflow.com/a/59961623
+func create(p string) (*os.File, error) {
+	if err := os.MkdirAll(filepath.Dir(p), 0770); err != nil {
+		 return nil, err
+	}
+	return os.Create(p)
+}
+
 func main() {
-	f, err := os.Create("test.md")
+	f, err := create("out/test.md")
 	
 	if err != nil {
 		log.Fatal(err)
